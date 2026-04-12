74c01b7873593d89a56761c494c5381a6e69c516
YanhLi-tran <1850365346@qq.com>
2026-04-12 15:49:38 +0800

    加入登录接口和API服务并测试成功

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,1 +1,15 @@
-//API服务器
+// API服务器
+package main
+
+import (
+	"AIWallHub/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.POST("/register", handlers.Register)
+	r.POST("/login", handlers.Login)
+	r.Run(":8080")
+}
--- handlers/auth.go
+++ handlers/auth.go
@@ -17,7 +17,7 @@ func Register(c *gin.Context) {
 	}
 	if err:=c.ShouldBindJSON(&json);err!=nil{
 		c.JSON(http.StatusBadRequest,gin.H{
-			"error":"参数格式错误"
+			"error":"参数格式错误",
 		})
 		return
 	}
@@ -26,12 +26,12 @@ func Register(c *gin.Context) {
 	usernamelen:=utf8.RuneCountInString(json.Username)
 	if usernamelen<3{
 		c.JSON(http.StatusBadRequest,gin.H{
-			"error":"用户名需要3-20个字符，当前用户名过短"
+			"error":"用户名需要3-20个字符,当前用户名过短",
 		})
 		return
 	}else if usernamelen>20{
 		c.JSON(http.StatusBadRequest,gin.H{
-			"error":"用户名需要3-20个字符，当前用户名过长"
+			"error":"用户名需要3-20个字符,当前用户名过长",
 		})
 		return
 	}
@@ -39,7 +39,7 @@ func Register(c *gin.Context) {
 	//校验密码长度
 	if len(json.Password)<6{
 		c.JSON(http.StatusBadRequest,gin.H{
-			"error":"密码至少需要6个字符"
+			"error":"密码至少需要6个字符",
 		})
 		return
 	}
@@ -48,7 +48,7 @@ func Register(c *gin.Context) {
 	passwordRegex:=regexp.MustCompile(`^[a-zA-Z0-9_]+$`)
 	if !passwordRegex.MatchString(json.Password){
 		c.JSON(http.StatusBadRequest,gin.H{
-			"error":"密码只能包含大小写字母、数字和下划线‘_’"
+			"error":"密码只能包含大小写字母、数字和下划线_",
 		})
 		return
 	}
@@ -57,7 +57,7 @@ func Register(c *gin.Context) {
 	emailRegex:=regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
 	if !emailRegex.MatchString(json.Email){
 		c.JSON(http.StatusBadRequest,gin.H{
-			"error":"邮箱格式不正确"
+			"error":"邮箱格式不正确",
 		})
 		return
 	}
@@ -65,7 +65,7 @@ func Register(c *gin.Context) {
 	//检查邮箱是否已被注册
 	if _,exists:=models.UserByEmail[json.Email];exists{
 		c.JSON(http.StatusConflict,gin.H{
-			"error":"该邮箱已被注册"
+			"error":"该邮箱已被注册",
 		})
 		return
 	}
@@ -85,5 +85,39 @@ func Register(c *gin.Context) {
 	c.JSON(http.StatusOK,gin.H{
 		"message":"您已成功注册，请记好密码",
 		"user_id":user.ID,
+	})
+
+}
+
+/*-----------------------------------------------------------------*/
+
+//登录接口(用邮箱方式)
+func Login(c *gin.Context){
+	var json struct{
+		Email string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	//解析JSON
+	if err:=c.ShouldBindJSON(&json);err!=nil{
+		c.JSON(http.StatusBadRequest,gin.H{
+			"error":"参数格式错误",
+		})
+		return
+	}
+
+	//登录信息检查
+	user,exists:=models.UserByEmail[json.Email]
+	if !exists || user.Password!=json.Password{
+		c.JSON(http.StatusUnauthorized,gin.H{
+			"error":"邮箱或密码错误",
+	})
+		return
+	}
+
+	c.JSON(http.StatusOK,gin.H{
+		"message":"登陆成功",
+		"user_id":user.ID,
+		"username":user.Name,
 	})
 }
